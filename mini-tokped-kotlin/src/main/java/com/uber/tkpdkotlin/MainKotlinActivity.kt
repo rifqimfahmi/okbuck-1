50eea6faa54a958cbbd6ae5b1ed4e7bbec6895f2
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-01-14 22:11:55 +0700

    Create mini tokped kotlin lib module

---
+++ mini-tokped-kotlin/src/main/java/com/uber/tkpdkotlin/MainKotlinActivity.kt
@@ -0,0 +1,13 @@
+package com.uber.tkpdkotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainKotlinActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main_kotlin)
+  }
+
+}
--- mini-tokped/src/main/java/com/uber/tkpd/activity/MainActivity.java
+++ mini-tokped/src/main/java/com/uber/tkpd/activity/MainActivity.java
@@ -1,9 +1,11 @@
 package com.uber.tkpd.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.uber.tkpd.R;
+import com.uber.tkpdkotlin.MainKotlinActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    findViewById(R.id.button).setOnClickListener(v -> {
+      Intent intent = new Intent(MainActivity.this, MainKotlinActivity.class);
+      startActivity(intent);
+    });
   }
 
 }
